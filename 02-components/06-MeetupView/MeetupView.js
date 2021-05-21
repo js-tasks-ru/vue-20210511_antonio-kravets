5f6fdbe308ad50a5490e188e7d22212d42b722ea
kriffer <antonio.kravets@gmail.com>
2021-05-21 21:36:25 +0300

    solution for 02/06

--- 02-components/06-MeetupView/MeetupView.js
+++ 02-components/06-MeetupView/MeetupView.js
@@ -1,29 +1,48 @@
-// import MeetupCover from './MeetupCover.js';
-// import MeetupDescription from './MeetupDescription.js';
-// import MeetupAgenda from './MeetupAgenda.js';
-// import MeetupInfo from './MeetupInfo.js';
-// import { getImageUrlByImageId } from './data.js';
+import MeetupCover from './MeetupCover.js';
+import MeetupDescription from './MeetupDescription.js';
+import MeetupAgenda from './MeetupAgenda.js';
+import MeetupInfo from './MeetupInfo.js';
+import {getImageUrlByImageId} from './data.js';
+
 
 const MeetupView = {
   name: 'MeetupView',
 
+  components: {
+    MeetupCover,
+    MeetupDescription,
+    MeetupAgenda,
+    MeetupInfo,
+  },
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    imageLink() {
+      return getImageUrlByImageId(this.meetup.imageId);
+    },
+  },
   template: `
     <div>
-      <!-- meetup cover -->
-      <div class="container">
-        <div class="meetup">
-          <div class="meetup__content">
-            <h3>Описание</h3>
-            <!-- meetup description -->
-
-            <h3>Программа</h3>
-            <!-- meetup agenda -->
-          </div>
-          <div class="meetup__aside">
-            <!-- meetup info -->
-          </div>
+    <meetup-cover :title="meetup.title" :link="imageLink"></meetup-cover>
+    <div class="container">
+      <div class="meetup">
+        <div class="meetup__content">
+          <h3>Описание</h3>
+          <meetup-description :description="meetup.description"></meetup-description>
+          <h3>Программа</h3>
+          <meetup-agenda :agenda="meetup.agenda"></meetup-agenda>
+        </div>
+        <div class="meetup__aside">
+          <meetup-info :organizer="meetup.organizer" :place="meetup.place" :date="new Date(meetup.date)"></meetup-info>
         </div>
       </div>
+    </div>
     </div>`,
 };
 
